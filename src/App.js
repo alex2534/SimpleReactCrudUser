469974a78648e8c889c57841c8895281494d9827
alex2534 <alex2534@outlook.com>
2022-03-02 12:38:27 -0300

    first Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import userList from "./Data";
+import UserTable from "./table/UserTable";
+import AddUserForm from "./form/AppUserForm";
+import EditUserForm from "./form/EditUserForm";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const initialUser = { id: null, name: "", username: "", age: "" };
+	const [users, setUsers] = useState(userList);
+	const [currentUser, setCurrentUser] = useState(initialUser);
+	const [editing, setEditing] = useState(false);
+
+	const addUser = (user) => {
+		user.id = users.length + 1;
+		setUsers([...users, user]);
+	};
+
+	const editUser = (id, user) => {
+		setEditing(true);
+		setCurrentUser(user);
+	};
+
+	const updateUser = (newUser) => {
+		setUsers(
+			users.map((user) => (user.id === currentUser.id ? newUser : user))
+		);
+	};
+
+	const deleteUser = (id) => setUsers(users.filter((user) => user.id !== id));
+	return (
+		<div className="container">
+			<h1>React CRUD App with Hooks</h1>
+			<div className="row">
+				<div className="five columns">
+					{editing ? (
+						<div>
+							<h2>Edit user</h2>
+							<EditUserForm
+								currentUser={currentUser}
+								setEditing={setEditing}
+								updateUser={updateUser}
+							/>
+						</div>
+					) : (
+						<div>
+							<h2>Add user</h2>
+							<AddUserForm addUser={addUser} />
+						</div>
+					)}
+				</div>
+				<div className="seven columns">
+					<h2>View users</h2>
+					<UserTable
+						users={users}
+						deleteUser={deleteUser}
+						editUser={editUser}
+					/>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Data.js
@@ -0,0 +1,14 @@
+const userList = [
+	{
+		id: 1,
+		name: "jhon",
+		username: "Doe",
+	},
+	{
+		id: 2,
+		name: "Jonny",
+		username: "Doe",
+	},
+];
+
+export default userList;
